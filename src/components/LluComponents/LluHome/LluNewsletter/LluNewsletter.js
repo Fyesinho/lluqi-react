a7002654e51ff1945437f52f26834e9cff608525
Rodrigo <rodrigo.gonzalez.zura@gmail.com>
2018-03-31 21:52:37 -0300

    Listo newsletter

---
+++ src/components/LluComponents/LluHome/LluNewsletter/LluNewsletter.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+//Assets
+import './LluNewsletter.css';
+import GiButton from "../../Common/GiButton";
+import LluIcon from "../../LluCommon/LluIcon/LluIcon";
+
+class LluNewsletter extends Component {
+    render() {
+        return (
+            <div className="llu-newsletter">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore </p>
+                <h1>¿Quieres recibir novedades de <span>Lluqi.com</span> en tu correo?</h1>
+                <p style={{marginBottom: "50px"}} className="container">Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div style={{display: "unset"}} className="form-inline">
+                    <input placeholder="tu correo electrónico" className="form-control form-group" type="text"/>
+                    <GiButton className="btn btn-primary">Suscribirme</GiButton>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LluNewsletter;
